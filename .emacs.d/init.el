6eaa92c5a7653a2c7dcbab49d75f95860b52bb67
Christian van Onzenoodt <christian.van-onzenoodt@uni-ulm.de>
2015-11-24 09:35:07 +0100

    add functions for font-size changing to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1048,6 +1048,42 @@ The FILE-NAME specifies the file name to search for."
 
 (load-ssh-agent-env)
 
+
+;; Changing font size
+(defun sanityinc/font-name-replace-size (font-name new-size)
+  (let ((parts (split-string font-name "-")))
+    (setcar (nthcdr 7 parts) (format "%d" new-size))
+    (mapconcat 'identity parts "-")))
+
+(defun sanityinc/increment-default-font-height (delta)
+  "Adjust the default font height by DELTA on every frame.
+Emacs will keep the pixel size of the frame approximately the
+same.  DELTA should be a multiple of 10, to match the units used
+by the :height face attribute."
+  (let* ((new-height (+ (face-attribute 'default :height) delta))
+         (new-point-height (/ new-height 10)))
+    (dolist (f (frame-list))
+      (with-selected-frame f
+        ;; Latest 'set-frame-font supports a "frames" arg, but
+        ;; we cater to Emacs 23 by looping instead.
+        (set-frame-font (sanityinc/font-name-replace-size
+                         (face-font 'default)
+                         new-point-height)
+                        t)))
+    (set-face-attribute 'default nil :height new-height)
+    (message "default font size is now %d" new-point-height)))
+
+(defun sanityinc/increase-default-font-height ()
+  (interactive)
+  (sanityinc/increment-default-font-height 10))
+
+(defun sanityinc/decrease-default-font-height ()
+  (interactive)
+  (sanityinc/increment-default-font-height -10))
+
+(global-set-key (kbd "s-+") 'sanityinc/increase-default-font-height)
+(global-set-key (kbd "s--") 'sanityinc/decrease-default-font-height)
+
 ;; #############################################################################
 ;; ################################# FUNCTIONS PATRICK #########################
 ;; #############################################################################
