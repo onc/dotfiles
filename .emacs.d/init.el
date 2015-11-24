0060a3ecea51fb22b062d88dbcd7cdb9617df2e3
Christian van Onzenoodt <christian.van-onzenoodt@uni-ulm.de>
2015-11-24 09:16:46 +0100

    remove jit-lock-defer-time from emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -121,8 +121,6 @@ re-downloaded in order to locate PACKAGE."
 ;; redraw completly before continue to avoid lagging
 (setq redisplay-dont-pause t)
 
-(setq jit-lock-defer-time 0.05)
-
 ;; Nonzero means echo unfinished commands after this many seconds of pause.
 ;; The value may be integer or floating point.
 (setq echo-keystrokes 0.02)
