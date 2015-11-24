a09c8ae72122f4513d541da60f71286ac8ba74b7
Christian van Onzenoodt <christian.van-onzenoodt@uni-ulm.de>
2015-11-24 09:21:57 +0100

    add leader-bindings for nerd-commenter and align in emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -229,7 +229,15 @@ re-downloaded in order to locate PACKAGE."
                           "1" 'highlight-symbol-at-point
                           "0" 'highlight-symbol-remove-all
                           "gst" 'magit-status
-                          "p" 'helm-projectile)))
+                          "p" 'helm-projectile
+                          "ci" 'evilnc-comment-or-uncomment-lines
+                          "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
+                          "ll" 'evilnc-quick-comment-or-uncomment-to-the-line
+                          "cc" 'evilnc-copy-and-comment-lines
+                          "cp" 'evilnc-comment-or-uncomment-paragraphs
+                          "cr" 'comment-or-uncomment-region
+                          "cv" 'evilnc-toggle-invert-comment-line-by-line
+                          "a" 'align-regexp)))
 
             (use-package evil-search-highlight-persist
               :ensure t
