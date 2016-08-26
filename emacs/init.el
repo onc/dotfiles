44b8f4e02b8f67be8f08d83f8baa17550dc0b369
Christian van Onzenoodt <christian.van-onzenoodt@uni-ulm.de>
2016-08-26 17:36:48 +0200

    Add function for running the current file

--- emacs/init.el
+++ emacs/init.el
@@ -1296,6 +1296,47 @@ The FILE-NAME specifies the file name to search for."
           (set-visited-file-name new-name)
           (set-buffer-modified-p nil))))))
 
+(defun onze-run-current-file ()
+  "Execute or compile the current file.
+For example, if the current buffer is the file x.pl,
+then it'll call “perl x.pl” in a shell.
+The file can be php, perl, python, ruby, javascript, bash, ocaml, vb, elisp.
+File suffix is used to determine what program to run.
+If the file is modified, ask if you want to save first.
+This command always run the saved version.
+If the file is Emacs Lisp, run the byte compiled version if exist."
+  (interactive)
+  (let (suffixMap fName fSuffix progName cmdStr)
+
+    ;; a keyed list of file suffix to comand-line program path/name
+    (setq suffixMap
+          '(("php" . "php")
+            ("py" . "python")
+            ("rb" . "ruby")
+            ("js" . "node")
+            ("sh" . "bash")))
+    (setq fName (buffer-file-name))
+    (setq fSuffix (file-name-extension fName))
+    (setq progName (cdr (assoc fSuffix suffixMap)))
+    (setq cmdStr (concat progName " \""   fName "\""))
+
+    (when (buffer-modified-p)
+      (progn
+        (when (y-or-n-p "Buffer modified. Do you want to save first?")
+          (save-buffer) ) ) )
+
+    (if (string-equal fSuffix "el") ; special case for emacs lisp
+        (progn
+          (load (file-name-sans-extension fName)))
+      (if progName
+          (progn
+            (message "Running…")
+            ;; (message progName)
+            (shell-command cmdStr "*run-current-file output*" ))
+        (message "No recognized program file suffix for this file.")))))
+
+
+
 (defun kill-this-buffer-if-not-modified ()
   "Kill current buffer, even if it has been modified."
   (interactive)
