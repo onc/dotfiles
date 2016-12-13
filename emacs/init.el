de7de442d35ea048872dff9ddd389f44904a8b09
Christian van Onzenoodt <c.onzenoodt@gmail.com>
2016-12-13 13:00:42 +0100

    Emojis in emacs 😍

--- emacs/init.el
+++ emacs/init.el
@@ -97,8 +97,6 @@
                     :family onc/font-family :height onc/font-size)
 (set-face-attribute 'variable-pitch nil
                     :family onc/font-family :height onc/font-size :weight 'regular)
-;; Emoji font
-(set-fontset-font t 'symbol (font-spec :family onc/emoji-font-family) nil 'prepend)
 
 ;; Show system name and full file path in emacs frame title
 (setq frame-title-format
@@ -541,6 +539,26 @@
   (use-package company-statistics
     :ensure t
     :config (company-statistics-mode)))
+
+
+;; Emojis completion like Github/Slack
+(use-package company-emoji
+  :ensure t
+  :config
+  (add-to-list 'company-backends 'company-emoji)
+  (defun --set-emoji-font (frame)
+    "Adjust the font settings of FRAME so Emacs can display emoji properly."
+    (if (eq system-type 'darwin)
+        ;; For NS/Cocoa
+        (set-fontset-font t 'symbol (font-spec :family "Apple Color Emoji") frame 'prepend)
+      ;; For Linux
+      (set-fontset-font t 'symbol (font-spec :family "Symbola") frame 'prepend)))
+
+  ;; For when Emacs is started in GUI mode:
+  (--set-emoji-font nil)
+  ;; Hook for when a frame is created with emacsclient
+  ;; see https://www.gnu.org/software/emacs/manual/html_node/elisp/Creating-Frames.html
+  (add-hook 'after-make-frame-functions '--set-emoji-font))
 
 
 ;; Snippets
