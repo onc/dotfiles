49e9b33436bcf84fe255cdc6fc11deee32070864
Christian van Onzenoodt <c.onzenoodt@gmail.com>
2017-01-30 19:45:58 +0100

    Removing unused `company-emoji` mode config

--- emacs/init.el
+++ emacs/init.el
@@ -580,21 +580,8 @@
 ;; Emojis completion like Github/Slack
 (use-package company-emoji
   :ensure t
-  :preface
-  (defun --set-emoji-font (frame)
-    "Adjust the font settings of FRAME so Emacs can display emoji properly."
-    (if (eq system-type 'darwin)
-        ;; For NS/Cocoa
-        (set-fontset-font t 'symbol (font-spec :family "Apple Color Emoji") frame 'prepend)
-      ;; For Linux
-      (set-fontset-font t 'symbol (font-spec :family "Symbola") frame 'prepend)))
-  :config
-  (add-to-list 'company-backends 'company-emoji)
-  ;; For when Emacs is started in GUI mode:
-  (--set-emoji-font nil)
-  ;; Hook for when a frame is created with emacsclient
-  ;; see https://www.gnu.org/software/emacs/manual/html_node/elisp/Creating-Frames.html
-  (add-hook 'after-make-frame-functions '--set-emoji-font))
+  :init
+  (add-to-list 'company-backends 'company-emoji))
 
 
 ;; Snippets
