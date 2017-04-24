574580e6a5dd0d42945f57ddaa21048f82a05c72
Christian van Onzenoodt <c.onzenoodt@gmail.com>
2017-04-24 14:26:10 +0200

    Reindent file

--- emacs/init.el
+++ emacs/init.el
@@ -572,7 +572,9 @@
   (defvar company-mode/enable-yas t
     "Enable yasnippet for all backends.")
   (defun company-mode/backend-with-yas (backend)
-    (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
+    (if (or
+         (not company-mode/enable-yas)
+         (and (listp backend) (member 'company-yasnippet backend)))
         backend
       (append (if (consp backend) backend (list backend))
               '(:with company-yasnippet))))
