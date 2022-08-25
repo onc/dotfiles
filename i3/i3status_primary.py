44bdfce296f08c7714730511c846fefbfe90ff07
Christian van Onzenoodt <christian.van-onzenoodt@uni-ulm.de>
2022-08-25 14:07:56 +0200

    Fix i3 config

--- i3/i3status_primary.py
+++ i3/i3status_primary.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from i3pystatus import Status
-from lifebar import Lifebar
 
 # Colors - base16:
 white = "#E0E0E0"
@@ -40,36 +39,9 @@ status.register("shell",
                 command="echo ; cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor",
                 interval=5)
 
-status.register("battery",
-                format="{consumption:.0f}W {percentage:.0f}% {remaining:%E%hh:%Mm}",
-                alert=True,
-                alert_percentage=5,
-                status={
-                    "DIS": "",
-                    "CHR": "",
-                    "FULL": "",
-                },
-                color=orange,
-                full_color=white,
-                charging_color=green,
-                critical_color=red)
-
-status.register(Lifebar,
-                color=white,
-                warn_color=orange,
-                alert_color=red,
-                charging_color=green,
-                steps=7)
-
 status.register("network",
-                interface="enp0s25",
+                interface="eno1",
                 format_up="{v4cidr}",
-                color_up=white,
-                color_down=gray)
-
-status.register("network",
-                interface="wlp3s0",
-                format_up=" {essid}/{v4} {quality:03.0f}%",
                 color_up=white,
                 color_down=gray)
 
@@ -77,15 +49,5 @@ status.register("pulseaudio",
                 format=" {volume}%",
                 color_unmuted=white,
                 color_muted=gray)
-
-status.register("backlight",
-                format=" {brightness}/{max_brightness}",
-                interval=1,
-                color=white)
-
-status.register("shell",
-                color=white,
-                command="echo ; /mnt/hdd/dotfiles/pacUpdates.sh",
-                interval=600)
 
 status.run()
