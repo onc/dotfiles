0c33412331a11c5775c29fa1e87d1048c49d2718
Christian van Onzenoodt <christian.vanonzenoodt@liebherr.com>
2024-12-23 19:33:53 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+# -*- mode: sh; coding: utf-8; -*-
+# vim:set filetype=sh fileencoding=utf-8:
+
+DIR=$(cd -P "$( dirname "BASH_SOURCE[0]" )" >/dev/null 2>&1 && pwd)
+
+function symlink_dotfile() {
+    file_name=$1
+
+    if [ -f ~/.$file_name ]; then
+        echo "Backing up ${file_name} to ${file_name}.back"
+        mv ~/.$file_name ~/.$file_name.back
+    fi
+
+    ln -s $DIR/$file_name ~/.$file_name
+}
+
+symlink_dotfile "zshrc"
+symlink_dotfile "zprofile"
+symlink_dotfile "vimrc"
+symlink_dotfile "tmux.conf"
+symlink_dotfile "npmrc"
+symlink_dotfile "curlrc"
